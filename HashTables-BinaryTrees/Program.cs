a947e9efa8d79b71b57ed0197febc05508395c5e
duvvamounika <mounikaduvva05@gmail.com>
2022-04-15 10:34:18 +0530

    Add BinaryTreeInsertUC1 branch and program

--- HashTables-BinaryTrees/Program.cs
+++ HashTables-BinaryTrees/Program.cs
@@ -11,30 +11,12 @@ namespace HashTables_BinaryTrees
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to HashTables and BinaryTrees");
-            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-            CountNumbOfOccurence(paragraph);
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
             Console.ReadLine();
-        }
-        public static void CountNumbOfOccurence(string paragraph)
-        {
-            MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
-
-            string[] words = paragraph.Split(' ');
-
-            foreach (string word in words)
-            {
-                if (hashTabe.Exists(word.ToLower()))
-                    hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
-                else
-                    hashTabe.Add(word.ToLower(), 1); //to,1 
-            }
-            Console.WriteLine("Displaying after add operation");
-            hashTabe.Display();
-            string s = "avoidable";
-            hashTabe.Remove(s);
-            Console.WriteLine("After removed an item {0}", s);
-            hashTabe.Display();
         }
     }
 }
-
+       
